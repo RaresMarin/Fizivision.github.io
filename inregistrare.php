118034dc679a5334c1d80dd4431ac6f72a9af43a
Rareș Mărincaș <rares.marincas@lucaciu.ro>
2025-05-04 15:28:14 +0300

    bici4

--- inregistrare.php
+++ inregistrare.php
@@ -2,39 +2,31 @@
 <html>
     <head>
     <link rel="icon" href="Fv.png" type="image/jpg">
-        <style>
-            nav li a{
-    text-decoration: none;
-    color:blueviolet;
-    font-size:20px;
-    text-align: center;
-    display:inline;
-
-}
-
-nav li{
-    height:70px;
-    width:10px;
-    text-align: center;
-    display:inline;
-
-}
-
-nav li a:hover{
-    color:#90bd2a;
-    display:inline;
-    background-color: #29104a;
-}
-
-a{
-    text-decoration: none;
-    color:white;
-}
-
-a:hover{
-    color:azure;
-}
-        </style>
+    <link rel="stylesheet" href="Fizivision.css">
+    <style>
+    
+    li a{
+        color:white;
+        display:inline-block;
+        width:8%;
+        
+    }
+    li {
+        display:inline;
+    }
+    input{
+        display:block;
+        margin:0 auto;
+    }
+    label{
+        color:pink;
+        font-size:20px;
+    }
+    button{
+        height:60px;
+        float:right;
+    }
+    </style>
     </head>
 <body style="background-color:#29104a">
 <li><a href="Fizivision.html">Acasă</a></li>
@@ -44,10 +36,26 @@ a:hover{
             <li><a href="Electri.html">Electricitate</a></li>
             <li><a href="LaboDigi.html">Laborator Digital</a></li>
             <li><a href="Contabili.html">Contact</a></li>
-<div style="color:white;text-align:center;font-size:20px;">
+
+<div style="color:white;text-align:center;font-size:20px;margin-top:100px;color:pink;">
 Bun venit pe Fizivision, <?php echo $_POST["Nume"]; ?><br>
 Adresa ta de email este <?php echo $_POST["Email"]; ?><br>
 Parola ta este <?php echo $_POST["Parolă"]; ?>
 </div>
+<h2>Rewiev</h2>
+<form action="note.php" method="post">
+<label>Aspectul site-ului</label>
+<input type="number" name="aspect" placeholder="0" min="0" max="10" id="aspect"></input>
+<br>
+<label>Calitatea informațiilor</label>
+<input type="number" name="calitate" placeholder="0" min="0" max="10" id="calitate"></input>
+<br>
+<label>Experimentelor</label>
+<input type="number" name="experimente" placeholder="0" min="0" max="10" id="experimente"></input>
+<br>
+<label>Varietatea Quizurilor</label>
+<input type="number" name="varietate" placeholder="0" min="0" max="10" id="varietate"></input>
+<button style="border-radius:20px;background-color:blueviolet;color:pink">Trimite note</button>
+</form>
 </body>
 </html>
---
+++ note.php
@@ -0,0 +1,65 @@
+<html>
+    <head>
+    <link rel="icon" href="Fv.png" type="image/jpg">
+    <link rel="stylesheet" href="Fizivision.css">
+    <style>
+        p{
+            font-size:20px;
+            text-align:center;
+            display:block;
+            width:98%;
+        }
+    li a{
+        color:white;
+        display:inline-block;
+        width:8%;
+        
+    }
+    li {
+        display:inline;
+    }
+    </style>
+    </head>
+    <body>
+    <nav>
+<li><a href="Fizivision.html">Acasă</a></li>
+            <li><a href="Meca.html">Mecanică</a></li>
+            <li><a href="Termodina.html">Termodinamică</a></li>
+            <li><a href="Opti.html">Optică</a></li>
+            <li><a href="Electri.html">Electricitate</a></li>
+            <li><a href="LaboDigi.html">Laborator Digital</a></li>
+            <li><a href="Contabili.html">Contact</a></li>
+</nav>
+<div style="color:pink;text-align:center;font-size:20px;margin-top:100px;">
+<?php
+$n=$_POST['aspect'];
+$m=$_POST['calitate'];
+$p=$_POST['experimente'];
+$q=$_POST['varietate'];
+$maxim=-1;
+$minim=11;
+$numbers = [$n , $m, $p , $q];
+foreach($numbers as $number)
+{
+    if($number>$maxim)
+    $maxim=$number;
+    if($number<$minim)
+    $minim=$number;
+}
+$suma=$n+$m+$p+$q;
+$media = $suma/4;
+echo "Nota totala pentru site este" . " ".  $media . ".";
+if($media<5)
+echo " Ne pare rău pentru experiența dumneavoastră! Vom încerca să îmbunătățim serviciile!";
+elseif ($media<8)
+echo " Echipa fizivision își dă toată silința să vă aducă cele mai interesante actualizări în viitorul apropiat!";
+else
+echo " Mulțumim pentru note! Ne bucurăm că va plăcut!";
+?>
+<br>
+<p>Nota maximă a fost <?php echo $maxim?>.</p>
+<p>Nota minimă a fost <?php echo $minim?>.</p>
+<progress min="0" max="40" value="<?php echo $suma?>"></progress>
+</div>
+</body>
+</html>
